9632b8463ab2aade521f1a073e44218b4da3695c
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-18 00:41:14 -0300

    feat: validate newCustomer data in createCustomer function

--- src/controllers/customersController.ts
+++ src/controllers/customersController.ts
@@ -1,1 +1,16 @@
-export async function createNewCustomer(req: Request, res: Response) {}
+import { Request, Response } from "express";
+import InvalidDataError from "@/errors/InvalidDataError";
+import newCustomerSchema from "@/schemas/newCustomerSchema";
+import httpStatus from "http-status";
+
+export async function createNewCustomer(req: Request, res: Response) {
+  const newTool = req.body;
+  const validation = newCustomerSchema.validate(newTool);
+  if (!!validation.error) {
+    throw new InvalidDataError(
+      "body",
+      validation.error.details.map((error) => error.message)
+    );
+  }
+  res.status(httpStatus.CREATED);
+}
