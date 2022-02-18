c88ae71da7bf79d38a291b1301ce35b48dc451c5
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-18 00:14:26 -0300

    feat: middleware to handle errors

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,16 @@ import "express-async-errors";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import errorHandlingMiddleware from "@/middlewares/errorHandlingMiddleware";
+import router from "@/routers";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
+
+app.use(errorHandlingMiddleware);
 
 export async function init() {
   await mongoose.connect(process.env.DATABASE_URL, {
