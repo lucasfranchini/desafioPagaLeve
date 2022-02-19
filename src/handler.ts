bed3eb8c20597f1b3740cc970f061baa5cd149eb
lucas franchini <lucasfranchini.a@gmail.com>
2022-02-19 17:57:25 -0300

    feat: deploy in aws lambda

---
+++ src/handler.ts
@@ -0,0 +1,26 @@
+import serverlessExpress from "@vendia/serverless-express";
+import app from "@/app";
+
+let serverlessExpressInstance;
+
+function asyncTask() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("connected to database"), 1000);
+  });
+}
+
+async function setup(event, context) {
+  const asyncValue = await asyncTask();
+  console.log(asyncValue);
+  serverlessExpressInstance = serverlessExpress({ app });
+  return serverlessExpressInstance(event, context);
+}
+
+function handler(event, context) {
+  if (serverlessExpressInstance)
+    return serverlessExpressInstance(event, context);
+
+  return setup(event, context);
+}
+
+export default handler;
